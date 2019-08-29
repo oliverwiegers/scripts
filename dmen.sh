04736db3866befbaff51882291e198483d759618
oliverwiegers <oliver.wiegers@gmail.com>
2019-08-29 16:37:09 +0200

    change color

--- dmen.sh
+++ dmen.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color0"
