63993798b96d6d157a3fc91f61d4aa7d45dfb337
oliverwiegers <oliver.wiegers@gmail.com>
2021-03-20 19:42:11 +0100

    Add cronjob gui launcher

--- command_line_parsing_dummy.sh
+++ command_line_parsing_dummy.sh
@@ -1,35 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# A POSIX variable.
-# Reset in case getopts has been used previously in the shell.
-OPTIND=1
+set -euo pipefail;
 
 # Print usage.
 print_usage() {
     usage="$(basename "$0") [OPTIONS]
 
-    Dummy message.
+    Dummy message
 
-        -h          show this usage.
-        -u  TEXT    do stuff."
+        -u  TEXT    do stuff.
+        "
 
     printf '%s\n' "${usage}"
 }
 
+run() {
+    # A POSIX variable.
+    # Reset in case getopts has been used previously in the shell.
+    OPTIND=1
+    
+    # Parse command line arguments.
+    while getopts "h?u:" opt; do
+        case "$opt" in
+        h)
+            print_usage
+            exit 0
+            ;;
+        u)
+            arg=$OPTARG
+            print '%s\n' "${arg}"
+            ;;
+        *)
+            print_usage
+            exit 1
+            ;;
+        esac
+    done
+}
 
-# Parse command line arguments.
-while getopts "h?u:" opt; do
-    case "$opt" in
-    h)
-        print_usage
-        exit 0
-        ;;
-    u)
-        arg=$OPTARG
-        ;;
-    *)
-        print_usage
-        exit 1
-        ;;
-    esac
-done
+run "$@"
