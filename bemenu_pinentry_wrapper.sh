7a9e4fbbb16d3a830d8ca7ae650c54507d77fa4c
oliverwiegers <oliver.wiegers@gmail.com>
2021-04-29 21:34:48 +0200

    Add bemenu wrapper scripts
    Wrapper scripts to add right colors and set needed opts.

---
+++ bemenu_pinentry_wrapper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Import the colors
+# shellcheck source=/home/oliverwiegers/.cache/wal/colors.sh
+. "${HOME}/.cache/wal/colors.sh"
+
+pinentry_bemenu \
+     --line-height 40 \
+     --tb "${color0}" --tf "${color4}" \
+     --fb "${color0}" --ff "${color8}" \
+     --nb "${color0}" --nf "${color8}" \
+     --hb "${color0}" --hf "${color4}" \
+     --tb "${color0}" --tf "${color1}" \
+     --sb "${color8}" --tf "${color4}"
