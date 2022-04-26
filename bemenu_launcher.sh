f3d65e151f704689eb228c3522e08fc3cce4ef79
oliverwiegers <oliver.wiegers@gmail.com>
2022-04-26 21:41:27 +0200

    Make bemenu launcher ubuntu ready

--- bemenu_launcher.sh
+++ bemenu_launcher.sh
@@ -4,12 +4,16 @@
 # shellcheck source=/home/oliverwiegers/.cache/wal/colors.sh
 . "${HOME}/.cache/wal/colors.sh"
 
- bemenu-run \
-     --ignorecase \
-     --line-height 40 \
-     --tb "${color0}" --tf "${color4}" \
-     --fb "${color0}" --ff "${color8}" \
-     --nb "${color0}" --nf "${color8}" \
-     --hb "${color0}" --hf "${color4}" \
-     --tb "${color0}" --tf "${color1}" \
-     --sb "${color8}" --tf "${color4}"
+if grep -q Ubuntu /etc/issue; then
+    wofi --show run
+else
+    bemenu-run \
+        --ignorecase \
+        --line-height 40 \
+        --tb "${color0}" --tf "${color4}" \
+        --fb "${color0}" --ff "${color8}" \
+        --nb "${color0}" --nf "${color8}" \
+        --hb "${color0}" --hf "${color4}" \
+        --tb "${color0}" --tf "${color1}" \
+        --sb "${color8}" --tf "${color4}"
+fi
