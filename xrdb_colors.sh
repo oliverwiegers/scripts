e7e26e02866b188cc1f33d9c852bdc5dc992d3bd
oliverwiegers <oliver.wiegers@gmail.com>
2023-04-30 08:14:14 +0200

    Beautify xrdb color show script

--- xrdb_colors.sh
+++ xrdb_colors.sh
@@ -4,15 +4,15 @@ read -r -d '' -a colors \
     < <( xrdb -query | sed -n 's/.*color\([0-9]\)/\1/p' | sort -nu | cut -f2)
 
 printf '\e[1;37m
- Black      Red        Green      Yellow     Blue       Magenta    Cyan       White
+ Black   Red     Green   Yellow  Blue    Magenta Cyan    White
 ───────────────────────────────────────────────────────────────────────────────────────\e[0m\n'
 
 for color in {0..7}; do
-    printf "\e[$((30+color))m █ %s \e[0m" "${colors[color]}"
+    printf "\e[$((30+color))m █ %s \t\e[0m" "${colors[color]}"
 done
 printf '\n'
 
 for color in {8..15}; do
-    printf "\e[1;$((22+color))m █ %s \e[0m" "${colors[color]}"
+    printf "\e[1;$((22+color))m █ %s \t\e[0m" "${colors[color]}"
 done
 printf '\n'
